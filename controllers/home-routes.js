912f3dfeec9d013883a47bb6b556960b172567f7
Spartacus <k_morter@outlook.com>
2020-07-25 15:05:16 -0600

    added post layout to homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,51 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+    Post.findAll({
+    attributes: [
+        'id',
+        'title',
+        'content',
+        'created_at',
+    ],
+    include: [
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+            {
+            model: User,
+            attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/', (req, res) => {
+    res.render('homepage', {
+        id: 1,
+        title: 'Handlebars Docs',
+        content: 'This blog post is about the handlebars docs.',
+        created_at: new Date(),
+        comments: [{}, {}],
+        user: {
+          username: 'test_user'
+        }
+      });
 });
 
 module.exports = router;
