2faff86447040baafbe64647309ccba4f8818803
Spartacus <k_morter@outlook.com>
2020-07-25 13:17:45 -0600

    added user authentication and login

--- models/User.js
+++ models/User.js
@@ -1,45 +1,61 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define User table columns and configuration
 User.init(
-  {
-    id: {
+    {
+      id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
-    },
-    username: {
+      },
+      username: {
         type: DataTypes.STRING,
         allowNull: false
-    },
-    email: {
+      },
+      email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
         validate: {
-            isEmail: true
+          isEmail: true
         }
-    },
-    password: {
+      },
+      password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-            len: [4]
+          len: [4]
         }
+      }
+    },
+    {
+      hooks: {
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
     }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user'
-  }
-);
+  );
 
 module.exports = User;
